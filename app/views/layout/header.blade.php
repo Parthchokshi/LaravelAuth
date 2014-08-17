38365c88201971257213b67431046c5805ec7ec4
Parth Chokshi <Parthchokshi1@gmail.com>
2014-08-18 04:40:56 +0530

    Create header.blade.php

---
+++ app/views/layout/header.blade.php
@@ -0,0 +1,13 @@
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 
+{{ HTML::style('../css/foundation.css') }}
+{{ HTML::style('../css/style.css') }}
+
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,400italic,300,300italic,100italic,100' rel='stylesheet' type='text/css'> 
+  
+<link rel="stylesheet" href="../css/normalize.css">
+
+<script src="../js/vendor/modernizr.js"></script>
+
